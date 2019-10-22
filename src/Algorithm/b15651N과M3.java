7c92628f3e0070ec5120c1484eaa8292bcdd828a
user <user@39.117.107.87>
2019-10-23 00:28:54 +0900

    N과M 1,2,3 연습

---
+++ src/Algorithm/b15651N과M3.java
@@ -0,0 +1,36 @@
+package Algorithm;
+
+import java.util.Scanner;
+
+public class b15651N과M3 {
+	static boolean[] c = new boolean[10];
+	static int[] a = new int[10];
+
+	static void go(int index, int n, int m) {
+		// index == m 출력
+		if (index == m) {
+			for (int i = 0; i < m; i++) {
+				System.out.print(a[i]);
+				if (i != m - 1)
+					System.out.print(' ');
+			}
+			System.out.println();
+			return;
+		} // 중복 없이 M개 뽑기 (오름차순)
+		
+		for(int i=1; i<=n ; i++) {
+			//if(c[i]) continue;
+			c[i] = true;
+			a[index] = i;
+			go(index+1,n,m);
+			c[i]=false;
+		}
+	}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+		go(0, n, m);
+	}
+
+}
---
+++ src/Algorithm/b15651N과M3_1.java
@@ -0,0 +1,39 @@
+package Algorithm;
+
+import java.util.*;
+
+public class b15651N과M3_1 {
+	static boolean[] c = new boolean[10];
+	static int[] a = new int[10];
+
+	static StringBuilder go(int index, int n, int m) {
+		// index == m 출력
+		if (index == m) {
+			StringBuilder sb = new StringBuilder();
+			for (int i = 0; i < m; i++) {
+				sb.append(a[i]);
+				if (i != m - 1)
+					sb.append(" ");
+			}
+			sb.append("\n");
+			return sb;
+		} // 중복 없이 M개 뽑기 (오름차순)
+		StringBuilder ans = new StringBuilder();
+		for (int i = 1; i <= n; i++) {
+			// if(c[i]) continue;
+			c[i] = true;
+			a[index] = i;
+			ans.append(go(index + 1, n, m));
+			c[i] = false;
+		}
+		return ans;
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+		System.out.print(go(0, n, m));
+	}
+
+}
